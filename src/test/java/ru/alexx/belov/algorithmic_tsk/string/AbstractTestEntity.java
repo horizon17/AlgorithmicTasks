6933bbba6e1da2e539aac329acba367e4c591e21
horizon17 <scsi9x@gmail.com>
2022-05-02 09:47:22 -0700

    Valid Palindrome

--- src/test/java/ru/alexx/belov/algorithmic_tsk/string/AbstractTestEntity.java
+++ src/test/java/ru/alexx/belov/algorithmic_tsk/string/AbstractTestEntity.java
@@ -6,17 +6,12 @@ import lombok.Data;
  * Working builder
  */
 @Data
-public class TestEntity {
+public class AbstractTestEntity {
 
     String string1;
-    String string2;
     boolean answer;
 
-    public TestEntity(String s1, String s2, boolean a) {
-        this.string1 = s1;
-        this.string2 = s2;
-        this.answer = a;
-    }
+
 
 
 
