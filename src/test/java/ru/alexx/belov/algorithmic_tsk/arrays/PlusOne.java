49c47440fe7686e1f19c7bddcc787778ad7ee9fa
horizon17 <scsi9x@gmail.com>
2022-06-04 16:41:20 -0700

    plus one

---
+++ src/test/java/ru/alexx/belov/algorithmic_tsk/arrays/PlusOne.java
@@ -0,0 +1,45 @@
+package ru.alexx.belov.algorithmic_tsk.arrays;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ *
+ */
+@QuarkusTest
+class PlusOne {
+
+    @Test
+    void process() {
+
+        // 0,1,2,4,5,7      // 0,2,3,4,6,8,9
+        int[] arr1 = new int [] {0,1,2,4,5,7};
+        System.out.println(Arrays.toString((plusOne(arr1))));
+
+
+
+    }
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1]; // да, весь массив всегда все 0, важна только его длина
+        digits[0] = 1;
+        return digits;
+    }
+
+
+
+
+}
